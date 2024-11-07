d68b5e6a56892d32068b25c47f6f72355bf674d4
CalziFrancesco <154509470+CalziFrancesco@users.noreply.github.com>
2024-11-07 18:52:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,19 @@
 
 function getRandomPokemonId() {
     const range = Math.random();
-    if (range < 0.5) {
+    if (range < 0.8) {
         return Math.floor(Math.random() * 1025) + 1;
     } else {
         return Math.floor(Math.random() * (10277 - 10001 + 1)) + 10001;
     }
+}
+
+function timeAlive () {
+    return Math.floor(Math.random() * (5000 - 3000 + 1)) + 3000;
+}
+
+function timeNotAlive() {
+    return Math.floor(Math.random() * (3000 - 1000 + 1)) + 1000;
 }
 
 async function fetchPokemonImage(pokemonId) {
@@ -28,6 +36,28 @@ async function fetchPokemonImage(pokemonId) {
         console.error('Errore nella fetch:', error);
     }
 }
+
+// function startLoop() {
+//     setInterval(() => {
+//         const randomPokemonId = getRandomPokemonId();
+//         fetchPokemonImage(randomPokemonId);
+//     }, 5000);
+// }
+
+function startLoop() {
+    async function loop() {
+        const randomPokemonId = getRandomPokemonId();
+        await fetchPokemonImage(randomPokemonId);
+        setTimeout(() => {
+            document.getElementById('pokemon-image').innerHTML = '';
+            setTimeout(loop, timeNotAlive());
+        }, timeAlive());
+    }
+
+    loop();
+}
+
+startLoop();
 
 const randomPokemonId = getRandomPokemonId();
 fetchPokemonImage(randomPokemonId);
