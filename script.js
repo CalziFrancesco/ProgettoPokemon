c5f438a34fe0ad3f3b36779f04c4546e7d2d0772
CalziFrancesco <154509470+CalziFrancesco@users.noreply.github.com>
2024-11-12 22:35:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,69 @@
 //da 1 a 1025, da 10001 a 10277
 
 function getRandomPokemonId() {
-    const range = Math.random();
-    if (range < 0.8) {
-        return Math.floor(Math.random() * 1025) + 1;
-    } else {
-        return Math.floor(Math.random() * (10277 - 10001 + 1)) + 10001;
-    }
+    return Math.floor(Math.random() * 1301) + 1;
 }
 
 function timeAlive () {
-    return Math.floor(Math.random() * (2000 - 1000 + 1)) + 1000;
+    return Math.floor(Math.random() * (8000 - 7000 + 1)) + 7000;
 }
 
 function timeNotAlive() {
     return Math.floor(Math.random() * (2000 - 1000 + 1)) + 1000;
 }
 
+async function isGifAvailable(url) {
+    try {
+        const response = await fetch(url, { method: 'HEAD' });
+        return response.ok;
+    } catch (error) {
+        return false;
+    }
+}
+
 async function fetchPokemonImage(pokemonId) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=1&offset=${pokemonId}`;
     
     try {
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error('Qualcosa non va');
         }
-        const data = await response.json();
-        const imageUrl = data.sprites.front_default;
-        const img = document.createElement('img');
-        img.src = imageUrl;
-        const nomePokemon = document.createElement('p');
-        nomePokemon.textContent = data.forms[0].name;
+        const pokemonResult = await response.json();
+        const pokemonInfoUrl = pokemonResult.results[0].url;
+        const pokemonInfoResponse = await fetch(pokemonInfoUrl);
+        const data = await pokemonInfoResponse.json();
         const imgPokeball = document.createElement('img');
-        imgPokeball.src = "PokeballSenzaSfondo.png";
-        //img.alt = `ID: ${pokemonId}`;
-        // document.getElementById('pokemon-image').innerHTML = '';
+        imgPokeball.src = "pokeball.png";
+        const pokemonName = data.forms[0].name;
+        const urlOver = `https://raw.githubusercontent.com/tdmalone/pokecss-media/master/graphics/pokemon/ani-front-shiny/${pokemonName}.gif`;
+
+        console.log("URL GIF:", urlOver);
+
+        const frontImageUrl = data.sprites.front_default;
+        const img = document.createElement('img');
+        img.src = frontImageUrl;
+        img.classList.add('cover-image');
+        const hoverImg = document.getElementById('hover-image');
+        // const imgPokeball = document.createElement('img');
+        // imgPokeball.src = "pokeball.png";
+        //hoverImg.src = urlOver; // URL della GIF dinamica
+        if (await isGifAvailable(urlOver)) {
+            hoverImg.src = urlOver;
+        } else {
+            hoverImg.src = frontImageUrl;
+        }
+
         document.getElementById('pokemon-image').appendChild(img);
-        document.getElementById('nome-pokemon').appendChild(nomePokemon);
+        document.getElementById('nome-pokemon').textContent = data.forms[0].name;
         document.getElementById('pokeball').appendChild(imgPokeball);
 
     } catch (error) {
         console.error('Errore nella fetch:', error);
     }
 }
+
+
 
 // function startLoop() {
 //     setInterval(() => {
@@ -59,6 +80,7 @@ function startLoop() {
             document.getElementById('pokemon-image').innerHTML = '';
             document.getElementById('nome-pokemon').innerHTML = '';
             document.getElementById('pokeball').innerHTML = '';
+            document.querySelector('.card').classList.add('hidden');
             setTimeout(loop, timeNotAlive());
         }, timeAlive());
     }
