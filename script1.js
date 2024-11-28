889e4aabccd12b9d7e7537b64891d350f550497b
MMala874 <147324950+MMala874@users.noreply.github.com>
2024-11-28 21:40:38 +0100

    Add files via upload

--- script1.js
+++ script1.js
@@ -11,6 +11,46 @@ function loadPokedex(){
     pokedex = JSON.parse(localStorage.getItem("savedPokemon")) || [];
     console.log("Pokedex caricata:", pokedex);
 }
+async function ShpokemonSrc(pokemon){
+  const pokemonContainer=document.getElementById("pokedex-container");
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            // Wrapper per immagine statica e hover
+            const wrapper = document.createElement('div');
+            wrapper.classList.add('wrapper');
+
+            const imgPokemon = document.createElement('img');
+            imgPokemon.src = pokemon.sprites.front_default;
+            imgPokemon.classList.add('pokemon-image');
+
+            const hoverImg = document.createElement('img');
+            hoverImg.classList.add('hover-image');
+            hoverImg.src="";
+            const shinyUrl = `https://raw.githubusercontent.com/tdmalone/pokecss-media/master/graphics/pokemon/ani-front-shiny/${pokemon.forms[0].name}.gif`;
+            hoverImg.src = (await esiste(shinyUrl)) ? shinyUrl : pokemon.sprites.front_default;
+            console.log("Visualizzando Pokémon:", pokemon);
+            console.log("Immagine statica:", pokemon.sprites.front_default);
+            console.log("GIF shiny URL:", shinyUrl);
+            console.log("Nome Pokémon:", pokemon.forms[0].name);
+
+            // Aggiungi immagini al wrapper
+            wrapper.appendChild(imgPokemon);
+            wrapper.appendChild(hoverImg);
+
+            // Nome del Pokémon
+            const nomePokemon = document.createElement('p');
+            nomePokemon.textContent = pokemon.forms[0].name;
+            nomePokemon.classList.add('nome-pokemon');
+
+            // Aggiungi wrapper e nome alla card
+            card.appendChild(wrapper);
+            card.appendChild(nomePokemon);
+
+            // Aggiungi la card al container
+            pokemonContainer.appendChild(card);
+}
+
 async function visualizzaPokemon() {
     const pokemonContainer = document.getElementById("pokedex-container");
     pokemonContainer.innerHTML = ''; // Pulisci il contenitore
@@ -111,4 +151,19 @@ document
       open = false;
     }
   });
+document.addEventListener("DOMContentLoaded",()=>{
+  const pokemonContainer = document.getElementById("pokedex-container");
+  const search=document.getElementById("poke-search")
+  search.addEventListener("input",()=>{
+    pokemonContainer.innerHTML = '';
+    const searchTerm = search.value.toLowerCase();
+    pokedex.forEach(pokemon=>{
+      const pokemonStr=pokemon.forms[0].name;
+      if(pokemonStr.includes(searchTerm)){
+        ShpokemonSrc(pokemon);
+      }
+    })
+  })
+})
+
 
